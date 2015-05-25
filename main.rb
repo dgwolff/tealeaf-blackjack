89699485627b32d465c5cdb83e3e9963d80dd94e
Dylan Wolff <dylanwolff@gmail.com>
2015-05-25 14:20:03 +1000

    Setup basic routes

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ use Rack::Session::Cookie, key:     "rack.session",
 
 helpers do
   def calculate_total(cards)
-    arr = cards.map { |e| e[0] }
+    arr = cards.map { |e| e[1] }
 
     total = 0
     arr.each do |value|
@@ -30,18 +30,38 @@ helpers do
 end
 
 get "/" do
-  erb :set_name
+  if session[:player_name]
+    redirect "/game"
+  else
+    redirect "/new_player"
+  end
 end
 
-post "/set_name" do
+get "/new_player" do
+  erb :new_player
+end
+
+post "/new_player" do
   session[:player_name] = params[:player_name]
   redirect "/game"
 end
 
 get "/game" do
-  session[:deck] = [["2", "H"], ["3", "D"]]
+  suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+  values = ["2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King", "Ace"]
+  session[:deck] = suits.product(values).shuffle!
+
+  session[:dealer_cards] = []
   session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
   session[:player_cards] << session[:deck].pop
 
   erb :game
 end
+
+
+
+
+
